b078b52b5e14056a9d33b1ce6ec794efa00afd17
Abbey Gilliland <abbey.gilliland@ocsbstudent.ca>
2021-03-25 17:05:09 +0000

    added comments.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// these blocks of code set up the variables. The number is made so that it is different every refresh.
 let number = Math.random() * 20 + 1
 number = parseInt(number)
 let guess = 0
-
+// This is a loop that only stops when the user correctly guesses the age. if the guess is lesser or more than the age, the user gets a message saying so, and the loop continues.
 while (guess !== number) {
   guess = prompt('Guess a number between 1 and 20')
   guess = parseInt(guess)
